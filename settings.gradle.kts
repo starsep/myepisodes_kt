cea8bb722bcb619f1d4437205fd3778ed432ebd7
Filip Czaplicki <github@starsep.com>
2024-01-20 12:14:47 +0100

    Upgrade refreshVersions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 rootProject.name = "myepisodes_kt"
 plugins {
-    // See https://jmfayard.github.io/refreshVersions
-    id("de.fayard.refreshVersions") version "0.51.0"
+    // See https://splitties.github.io/refreshVersions/
+    id("de.fayard.refreshVersions") version "0.60.3"
 }
 
